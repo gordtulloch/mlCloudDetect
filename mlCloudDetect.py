4197fdcd8fbf3467efb69f30c1732a2c08521ea8
gordtulloch <gord.tulloch@gmail.com>
2024-03-26 08:59:41 -0500

    Update mlCloudDetect.py

--- mlCloudDetect.py
+++ mlCloudDetect.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from keras.models import load_model  # TensorFlow is required for Keras to work
 from PIL import Image, ImageOps  # Install pillow instead of PIL
 import numpy as np
